81a17fde9b6f96287879d965b682cb1a95614eae
aliadamovich <alesya.argentina@gmail.com>
2024-11-06 14:04:12 -0300

    git

--- src/common/utils/handleNetworkError.ts
+++ src/common/utils/handleNetworkError.ts
@@ -1,23 +1,23 @@
-// import axios from "axios"
-// import { setAppError, setAppStatus } from "app/appSlice"
-// import { AppDispatch } from "app/store"
+import axios from "axios"
+import { setAppError, setAppStatus } from "app/appSlice"
+import { AppDispatch } from "app/store"
 
-// export const handleServerNetworkError = (dispatch: AppDispatch, err: unknown): void => {
-// 	let errorMessage = "Some error occurred"
+export const handleServerNetworkError = (dispatch: AppDispatch, err: unknown): void => {
+	let errorMessage = "Some error occurred"
 
-// 	// ❗Проверка на наличие axios ошибки
-// 	if (axios.isAxiosError(err)) {
-// 		// ⏺️ err.response?.data?.message - например получение тасок с невалидной todolistId
-// 		// ⏺️ err?.message - например при создании таски в offline режиме
-// 		errorMessage = err.response?.data?.message || err?.message || errorMessage
-// 		// ❗ Проверка на наличие нативной ошибки
-// 	} else if (err instanceof Error) {
-// 		errorMessage = `Native error: ${err.message}`
-// 		// ❗Какой-то непонятный кейс
-// 	} else {
-// 		errorMessage = JSON.stringify(err)
-// 	}
+	// ❗Проверка на наличие axios ошибки
+	if (axios.isAxiosError(err)) {
+		// ⏺️ err.response?.data?.message - например получение тасок с невалидной todolistId
+		// ⏺️ err?.message - например при создании таски в offline режиме
+		errorMessage = err.response?.data?.message || err?.message || errorMessage
+		// ❗ Проверка на наличие нативной ошибки
+	} else if (err instanceof Error) {
+		errorMessage = `Native error: ${err.message}`
+		// ❗Какой-то непонятный кейс
+	} else {
+		errorMessage = JSON.stringify(err)
+	}
 
-// 	dispatch(setAppError({ error: errorMessage }))
-// 	dispatch(setAppStatus({ status: "failed" }))
-// }
+	dispatch(setAppError({ error: errorMessage }))
+	dispatch(setAppStatus({ status: "failed" }))
+}
