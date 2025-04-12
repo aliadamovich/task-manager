303c372b743c2b9d7243d98e48b6af653e950418
aliadamovich <alesya.argentina@gmail.com>
2025-04-12 20:44:53 -0300

    fix: fix baseApi

--- src/app/baseApi.ts
+++ src/app/baseApi.ts
@@ -1,27 +1,5 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
-import { setAppError } from "./appSlice"
-import { ResultCode } from "common/enums/enum"
 import { handleError } from "common/utils/handleErrors"
-
-// export const baseApi = createApi({
-// 	reducerPath: "todolistsApi",
-// 	baseQuery: fetchBaseQuery({
-// 		baseUrl: process.env.REACT_APP_BASE_URL,
-// 		credentials: "include",
-// 		prepareHeaders: (headers) => {
-// 			headers.set("API-KEY", `${process.env.REACT_APP_API_KEY}`)
-// 			headers.set("Authorization", `Bearer ${process.env.REACT_APP_AUTH_TOKEN}`)
-// 		},
-// 	}),
-// 	endpoints: () => ({}),
-// 	tagTypes: ["Todolist", "Tasks"],
-// })
-
-// export const baseApi = createApi()(args, api, extraOptions)
-
-// 		return result
-// 	},
-// })
 
 export const baseApi = createApi({
 	baseQuery: async (args, api, extraOptions) => {
@@ -30,7 +8,7 @@ export const baseApi = createApi({
 			credentials: "include",
 			prepareHeaders: (headers) => {
 				headers.set("API-KEY", `${process.env.REACT_APP_API_KEY}`)
-				headers.set("Authorization", `Bearer ${process.env.REACT_APP_AUTH_TOKEN2}`)
+				headers.set("Authorization", `Bearer ${process.env.REACT_APP_AUTH_TOKEN}`)
 			},
 		})(args, api, extraOptions)
 		
